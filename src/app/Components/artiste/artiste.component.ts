948a4b1fe5089e4282158e78bb8fb8e85fb9cb90
Sinda Haboubi <sindahaboubi104@gmail.com>
2022-05-28 01:57:13 +0100

    design terminé

---
+++ src/app/Components/artiste/artiste.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artiste',
+  templateUrl: './artiste.component.html',
+  styleUrls: ['./artiste.component.css']
+})
+export class ArtisteComponent implements OnInit {
+
+  constructor() { } 
+
+
+  ngOnInit(): void {
+  }
+
+  
+
+}
